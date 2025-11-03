33e74c21e98a567b4e05ae015c90b33ecefe14ca
Luka Volarević <luka.volarevic@fer.hr>
2025-11-03 23:00:09 +0100

    Folder structure setup

---
+++ frontend/src/app/pages/landingPage/landing.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  // imports: [],
+  templateUrl: './landing.html',
+  styleUrl: './landing.css'
+})
+export class LandingPage {
+
+}
