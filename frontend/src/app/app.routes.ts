33eb458c62159f78ad6535dbbe8da9af36ccbb1d
Luka Volarević <luka.volarevic@fer.hr>
2025-12-11 11:55:56 +0100

    Removed the old profiles page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LandingPage } from './pages/landingPage/landing.page';
 import { LoginPage } from './pages/auth/login/login.page';
 import { RegisterPage } from './pages/auth/register/register.page';
 import { SwipePage } from './pages/swipe/swipe.page';
-import { ProfilePage } from './pages/profile/profile.page';
+// import { ProfilePage } from './pages/profile/profile.page';
 
 export const routes: Routes = [
   { path: '', component: LandingPage },
